e7896e99ac236f55b3e185445f87f55f8d0e0233
America-Miguel-Parra <isc19350307@gmail.com>
2023-10-13 16:26:04 -0600

    Fondo degradado-Inicio

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default function App() {
+  return (
+    <LinearGradient
+        colors={['#FFFFFF', '#1B396A']}
+        style={styles.container}
+        start={{ x: 1, y: 0 }}
+        end={{ x: 1, y: 1 }}
+      >
+        <Text>Control de Préstamo de Equipos Multimedia</Text>
+      </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
