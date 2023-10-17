68e5b09c0d5903758234b93bde82f077104d8fa3
America-Miguel-Parra <isc19350307@gmail.com>
2023-10-17 03:07:56 -0600

    Haciendo responsive la app en cualquier dispositivo

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import {  useFonts, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import { LinearGradient } from 'expo-linear-gradient';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 
 export default function App() {
   
@@ -26,19 +28,29 @@ export default function App() {
         style={{ width: 100, height: 100, marginBottom: 20 }}
         />
         
-        <Text style={{marginBottom: 20, color: '#A9A7AA'}}> ──────────────────────</Text>
+        <Text style={{marginBottom: 20, color: '#A9A7AA'}}> ────────────────────</Text>
 
-        <Text style={{ color: '#1B396A', fontFamily: 'Montserrat_700Bold', fontSize: 14, marginBottom: 430, width: 200, textAlign: "center" }}>Control de Préstamo de Equipos Multimedia</Text>
+        <Text style={{ color: '#1B396A', fontFamily: 'Montserrat_700Bold', fontSize: 14, marginBottom: 430, maxWidth: 200, textAlign: "center" }}>Control de Préstamo de Equipos Multimedia</Text>
+
+        <Ionicons.Button name="person" size={24} color ="white" borderRadius={20} alignItems= "center">Administrador</Ionicons.Button>
+      
       </LinearGradient>
       
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ 
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    
+  },
+  responsiveBox: {
+    width: '80%',
+    height: '50%',
+    backgroundColor: 'blue',
   }, 
 
 });
+
